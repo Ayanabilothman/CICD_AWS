eb1664b230dc3d9ab64d7fc2cd1bf89082773f1f
Ayanabilothman <aya.nabil.othman@gmail.com>
2026-06-21 22:52:21 +0300

    testmergepull

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('testbranch')
+  @Get('testmergepull')
   CD() {
     return 'Success CD';
   }
